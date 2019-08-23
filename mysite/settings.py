37cd6a518c991253017be55417512d595cae17f4
msohanifr <msohani_fr@yahoo.fr>
2019-08-23 11:05:19 -0400

    cart number of items added

--- mysite/settings.py
+++ mysite/settings.py
@@ -155,6 +155,16 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'mysite/static'),
 )
 
+# Heroku ----------------------------------------------------
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# ---------------- Heroku ------------------------------------
+
+
 LOGIN_REDIRECT_URL = 'update_profile'
 LOGOUT_REDIRECT_URL = 'home'
 HOME_REDIRECT_URL = 'home'
