ed69c316dbb5322740757ba3ce26a19367f8f504
msohani <msohani_fr@yahoo.fr>
2019-09-20 13:54:02 -0400

    cart number of items added

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,8 +84,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-"""
+
 DATABASES = {
+"""
     # 'default': {
     #   'ENGINE': 'django.db.backends.sqlite3',
     #   'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
@@ -107,9 +108,9 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
     },
-
-}
 """
+}
+
 
 
 # ----------------- START Heroku --------------------------------
