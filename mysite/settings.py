d8d5ebd2414903c81eec6ca10bd63e48bff6591b
msohani <msohani_fr@yahoo.fr>
2019-09-20 13:54:58 -0400

    cart number of items added

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,9 +84,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-
-DATABASES = {
 """
+DATABASES = {
     # 'default': {
     #   'ENGINE': 'django.db.backends.sqlite3',
     #   'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
@@ -108,15 +107,18 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
     },
-"""
-}
 
+}
+"""
 
 
 # ----------------- START Heroku --------------------------------
 import django_heroku
 import dj_database_url
 
+DATABASES = {
+
+}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # Heroku ----------------------------------------------------
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
